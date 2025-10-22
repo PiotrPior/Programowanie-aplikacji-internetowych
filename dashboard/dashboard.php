6c27d918bc5ed77a33019b6c8ed048749b16c47b
Piotr Pióro <piotrpioro@MacBook-Air-Piotr.local>
2025-10-22 08:50:57 +0200

    Dodano pliki z 2_strona do dashboard

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,1 +1,41 @@
+<?php
+session_start();
 
+if (!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dashboard.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <nav>
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+        <a href="#">Link 4</a>
+        <a href="#">Link 5</a>
+
+    <form action="logout.php" method="post" style="display: inline; float: right;">
+        <input type="submit" value="logout">
+    </form>
+    </nav>
+    <header>
+        <h1>Welcome to LOW KEY DEMO!</h1>
+    </header>
+</body>
+</html>
+<?php
+if (empty($_SESSION['login'])){
+    echo "Witaj! Zaloguj się.";
+} else {
+    echo "Witaj ".$_SESSION['login']."!";
+}
+?>
--- dashboard/login.php
+++ dashboard/login.php
@@ -1,1 +1,59 @@
+<?php
+session_start();
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login_demo";
+$isCorrect = true;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = $_POST['login'] ?? "";
+    $pass = $_POST['pass'] ?? "";
+    $_SESSION['login'] = $login;
+    $_SESSION['pass'] = $pass;
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "SELECT login, passwd FROM logins";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($login === $row['login'] && $pass === $row['passwd']) {
+                $isCorrect = true;
+                header("Location: dashboard.php");
+                exit;
+            }
+        }
+        if(!$isCorrect) {
+            echo "Błędny login lub hasło";
+        }
+    }
+
+    mysqli_close($conn);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl1.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>LOGIN</h1>
+    <form action="login.php" method="post">
+        Login: <input type="text" name="login" id="login">
+        <br><br>
+        Password: <input type="password" name="pass" id="pass">
+        <input type="submit" value="submit">
+    </form>
+    <a href="register.php">Register</a>
+</body>
+</html>
